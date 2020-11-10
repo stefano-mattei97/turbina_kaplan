1d48e1876c434e0a42f254d60ab420a0825c1ca7
Luana <colagiacomo.17553383@studenti.uniroma1.it>
2020-11-10 15:51:50 +0100

    modificato proff

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 #INPUT
 
-Q = 9.5                               #[m^3/s]
+Q = 95                               #[m^3/s]
 H = 5.6
 Np = 24
 efficiency = 0.85
@@ -59,8 +59,9 @@ tgv = (math.pi * Dgv) / Zgv                               #passo
 
 We = g * H                              #lavoro di Eulero
 
-step=200
-r = np.linspace(Di,De,step)
+step=11
+mid = 6
+r = np.linspace(Di,De,step,endpoint=True)
 #inizializzazione vettori velocità
 U = np.zeros(len(r))
 Cm = np.zeros(len(r))
@@ -74,7 +75,7 @@ phi = Q/(omega * (2 * r)**3)              #flow coefficient
 
 #Free vortex: Cm costante
 
-Cm = phi[100] * U[100]                    #componente assiale velocità assoluta (midspan)
+Cm = phi[mid] * U[mid]                    #componente assiale velocità assoluta (midspan)
 
 #Prova eq.Radiale
 
@@ -87,7 +88,7 @@ alpha1 = (np.arctan(k1/(r*Cm))/(2*3.14))*360                    #gli angoli sono
 alpha2 = (np.arctan(k2/(r*Cm))/(2*3.14))*360
 beta1 = (np.arctan((U/Cm)-(k1/(r*Cm))))/(2*3.14)*360
 Wu1 = U-Cu1
-Wu2 = U+Cu2
+Wu2 = U-Cu2
 data = {'U':U,
         'Cm':Cm,
         'Cu1':Cu1,
@@ -99,30 +100,31 @@ data = {'U':U,
         'alpha2':alpha2 }
 Database = pd.DataFrame (data, columns = ['U','Wu1','Wu2','Cm','Cu1','Cu2','beta1','alpha1','alpha2'])
 
-dato = Database.iloc[100]
-fig = plt.figure()
-ax = plt.subplot(111)
-ax.set_aspect(1)
 
-#IN
 
-ax.arrow(dato['U'],dato['Cm'],-dato['U'],0,length_includes_head=True,width=0.5,facecolor='y')
-ax.arrow(dato['U'],dato['Cm'],-dato['Cu1'],-dato['Cm'],length_includes_head=True,width=0.5,facecolor='b')
-ax.arrow(0,dato['Cm'],dato['Wu1'],-dato['Cm'],length_includes_head=True,width=0.5,facecolor='g')
-ax.annotate('U',(dato['U']*0.5, 1))
-ax.annotate('C1',(dato['U'], 0.2))
-ax.annotate('W1',(dato['U']/3.3, 0))
-plt.show()
 
-#OUT
-dato = Database.iloc[100]
-fig = plt.figure()
-ax = plt.subplot(111)
-ax.set_aspect(1)
-ax.arrow(0,dato['Cm'],dato['Wu2'],-dato['Cm'],length_includes_head=True,width=0.5,facecolor='g')
-ax.arrow(dato['Wu2'],0,-dato['U'],0,length_includes_head=True,width=0.5,facecolor='y')
-ax.arrow(0,dato['Cm'],dato['Cu2'],-dato['Cm'],length_includes_head=True,width=0.5,facecolor='b')
-ax.annotate('U',(dato['U']*0.5, 0))
-ax.annotate('C2',(0, 0.2))
-ax.annotate('W2',(dato['U']/2,1.2))
-plt.show()
+def triangoliVelocita(dato):
+
+        fig = plt.figure()
+        ax = plt.subplot(111)
+        ax.set_aspect(1)
+        #ax.arrow(dato['U'],dato['Cm'],-dato['U'],0,length_includes_head=True,width=0.5,facecolor='y')
+        ax.arrow(0, 0, dato['U'], 0, length_includes_head=True, width=0.5, facecolor='y')
+        ax.arrow(0,0,dato['Cu1'],dato['Cm'],length_includes_head=True,width=0.5,facecolor='b')
+        ax.arrow(dato['U'],0,-dato['Wu1'],dato['Cm'],length_includes_head=True,width=0.5,facecolor='g')
+        #ax.annotate('U',(dato['U']*0.5, 1))
+        #ax.annotate('C1',(dato['U'], 0.2))
+        #ax.annotate('W1',(dato['U']/3.3, 0))
+        #ax = plt.subplot(122)
+        #ax.set_aspect(1)
+        ax.arrow(dato['U'],0,-dato['Wu2'],dato['Cm'],length_includes_head=True,width=0.5,facecolor='g')
+        #ax.arrow(dato['Wu2'],0,-dato['U'],0,length_includes_head=True,width=0.5,facecolor='y')
+        ax.arrow(0,0,dato['Cu2'],dato['Cm'],length_includes_head=True,width=0.5,facecolor='b')
+        #ax.annotate('U',(dato['U']*0.5, 0))
+        #ax.annotate('C2',(0,-0.5))
+        #ax.annotate('W2',(dato['U']/2,1.2))
+        plt.show()
+        return()
+
+sezionemid =  Database.iloc[mid]
+triangoliVelocita(sezionemid)
