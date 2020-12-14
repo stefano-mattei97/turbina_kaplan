b36abcced1230aa08c34b596fa1863fce7a6b5b2
Eros <eros.scotto@gmail.com>
2020-12-14 21:07:03 +0100

    modifica bladedesign2

--- Progetto.py
+++ Progetto.py
@@ -109,13 +109,14 @@ Drafttubedb= Drafttube(Inputdb['Q'],Inputdb['H'],OperatingPointdb['Ns'],CanaleMe
 Drafttubedb=Drafttubedb.iloc[0]
 
 #Blade Design
-Bladedesigndb,chord = pd.DataFrame(data=None, columns=['CL','CD','AoA'])
+Bladedesigndb= pd.DataFrame(data=None, columns=['CL','CD','AoA'])
 lsezioni = [0,2,5,8,10]
+chordr=np.zeros(len(lsezioni))
 str=['6424','6424','6410','6410','6410']
 str2=['polarNaca6424','polarNaca6424','polarNaca6410','polarNaca6410','polarNaca6410']
 for jj in range(len(lsezioni)):
     sez=Database.iloc[lsezioni[jj]]
-    DFrame=BladeDesign2(sez,g,H,efficiency,rho,OperatingPointdb['Ns'],
+    DFrame,chordr[jj]=BladeDesign2(sez,g,H,efficiency,rho,OperatingPointdb['Ns'],
                         lsezioni,OperatingPointdb['Z'],CanaleMeridianodb['Di'],
                         str[jj],str2[jj],jj)
     Bladedesigndb = pd.concat([Bladedesigndb, DFrame],axis=0)
@@ -134,5 +135,5 @@ plt.show()
 
 #PRESTAZIONI
 Prestazionidb = Prestazioni(WE,Q,rho,OperatingPointdb['Omega'],Bladedesigndb['Wm'],Bladedesigndb['betam'],
-                            chord,CanaleMeridianodb['b'],Bladedesigndb['CL'], Bladedesigndb['CD'],
+                            chordr,CanaleMeridianodb['b'],Bladedesigndb['CL'], Bladedesigndb['CD'],
                             CanaleMeridianodb['De'],CanaleMeridianodb['Di'],CanaleMeridianodb['Hn'],Costantidb['g'])
